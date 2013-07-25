d160c2bbed90584a5db66121ae498b44b1bbdd9e
Peter Hickey <hickey@wehi.edu.au>
2013-07-25 15:51:49 +1000

    Updated testing.sh to include both queryname sorted and coordinate sorted BAM analyses

--- testing.sh
+++ testing.sh
@@ -33,8 +33,10 @@ samtools index CS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam
 
 samtools index CS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam
 
-#### Sort "new_flag" BAM by queryname ####
+#### Sort BAMs by queryname ####
 SortSam I=new_flag.SRR400564_1.fastq.gz_bismark_pe.bam O=QS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam SO=queryname TMP_DIR=${TMP_DIR}
+
+SortSam I=old_flag.SRR400564_1.fastq.gz_bismark_pe.bam O=QS_old_flag.SRR400564_1.fastq.gz_bismark_pe.bam SO=queryname TMP_DIR=${TMP_DIR}
 
 #### Run comethylation_v3.py on QS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam with different filters on overlapping mates from paired-end reads ####
 mkdir test_data/log
@@ -47,9 +49,14 @@ python comethylation_v3.py --nTuple 1 --methylationType CG --overlappingPairedEn
 
 python comethylation_v3.py --nTuple 1 --methylationType CG --overlappingPairedEndCheck none test_data/QS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam test_data/new_flag_filter=none &> test_data/log/new_flag_filter=none.log
 
-#### Run bismark_methylation_extractor and bismark2bedGraph with --comprehensive flag on CS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam and CS_old_flag.SRR400564_1.fastq.gz_bismark_pe.bam ####
+#### Run bismark_methylation_extractor and bismark2bedGraph with --comprehensive flag on CS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam and CS_old_flag.SRR400564_1.fastq.gz_bismark_pe.bam. Run it on both the coordianate sorted BAMs (CS_*.bam) and queryname sorted BAMs (QS_*.bam) to see if there is any difference  ####
 ./bismark_methylation_extractor -p --no_overlap --comprehensive --ignore 0 --ignore_r2 0 --merge_non_CpG -o test_data --bedGraph --counts --buffer_size 10G test_data/CS_old_flag.SRR400564_1.fastq.gz_bismark_pe.bam
 
 ./bismark_methylation_extractor -p --no_overlap --comprehensive --ignore 0 --ignore_r2 0 --merge_non_CpG -o test_data --bedGraph --counts --buffer_size 10G test_data/CS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam
 
+./bismark_methylation_extractor -p --no_overlap --comprehensive --ignore 0 --ignore_r2 0 --merge_non_CpG -o test_data --bedGraph --counts --buffer_size 10G test_data/QS_old_flag.SRR400564_1.fastq.gz_bismark_pe.bam
+
+./bismark_methylation_extractor -p --no_overlap --comprehensive --ignore 0 --ignore_r2 0 --merge_non_CpG -o test_data --bedGraph --counts --buffer_size 10G test_data/QS_new_flag.SRR400564_1.fastq.gz_bismark_pe.bam
+
 #### Explore results in R (see test_data/explore_methylation_calling.r) ####
+
